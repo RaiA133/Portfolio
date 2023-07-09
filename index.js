8eb4d9567302ef156a19f1104fbb86d8f282386a
RaiA133 <rai010303@gmail.com@gmail.com>
2023-07-10 01:05:23 +0700

    scroll animate & gitignore

---
+++ index.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements_left = document.querySelectorAll('.hidden-left');
+hiddenElements_left.forEach((el) => observer.observe(el));
